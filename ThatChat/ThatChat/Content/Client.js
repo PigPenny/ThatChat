be542287a743a611afb17caa2f5331313d95fc2a
Connor Goudie <connorgoudie@live.com>
2017-11-22 22:03:06 -0800

    Fixed styling error with nav staying open

--- ThatChat/ThatChat/Content/Client.js
+++ ThatChat/ThatChat/Content/Client.js
@@ -1,9 +1,10 @@
-﻿
+﻿var open = false;
 function openSide() {
     document.getElementById("mySideNav").style.width = "260px";
     document.getElementById("mySideNav").style.display = "block";
     document.getElementById("mySideNav").style.height = "100%";
     document.getElementById("mySideNav").style.left = "0px";
+    open = true;
 }
 
 
@@ -11,13 +12,16 @@ function closeSide() {
     if ($(document).width() <= 991) {
         document.getElementById("mySideNav").style.width = "0";
         document.getElementById("mySideNav").style.left = "-30px";
+        open = false;
     }
 }
 
 $(window).resize(function () {
     if ($(document).width() > 991) {
         document.getElementById("mySideNav").style.left = "-30px";
-    } else {
+    } else if (!open) {
         document.getElementById("mySideNav").style.width = "0";
+    } else {
+        document.getElementById("mySideNav").style.left = "0px";
     }
 });
